3ad9a78e708e0cf34fcefb9449f7c80f34381a61
cruiseqe2 <cruiseqe2@gmail.com>
2024-09-06 15:01:53 +0100

    Initial Commit

---
+++ Custom Numeric Keypad/Custom_Numeric_KeypadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Numeric_KeypadApp.swift
+//  Custom Numeric Keypad
+//
+//  Created by Mark Oelbaum on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Numeric_KeypadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
